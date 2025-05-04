9ec2e0bc2625083777f526f470e48618954982e4
Roni dev <ronidev39@gmail.com>
2025-05-04 20:48:56 +0600

    small change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,15 @@ import { SkillSection } from "../components/UI/SkillSection";
 import { Hero } from "../components/UI/Hero";
 import Protfolio from "../components/UI/Protfolio";
 import InternshipSection from "../components/UI/InternshipSection";
+import { Allskills } from "../components/UI/Allskills";
 
 export const Home = () => {
   return (
     <>
       <Hero />
       <SkillSection />
+      <Allskills />
+
       <section className="px-2 lg:pl-0">
         <Protfolio />
       </section>
